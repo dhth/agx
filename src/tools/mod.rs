74837a8cd75f44c1b2b377b49a5332fe704ea71d
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-12-17 23:53:15 +0100

    add run_cmd tool

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -3,8 +3,10 @@ mod edit_file;
 mod helpers;
 mod read_dir;
 mod read_file;
+mod run_cmd;
 
 pub use create_file::*;
 pub use edit_file::*;
 pub use read_dir::*;
 pub use read_file::*;
+pub use run_cmd::*;
