897d591babbb56c19010401e040117fcfda4fa41
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2026-01-01 05:31:38 +0530

    add favicon (#23)

--- src/debug/server.rs
+++ src/debug/server.rs
@@ -18,6 +18,7 @@ const EVENTS_PATH: &str = "/api/debug/events";
 const ROOT_HTML: &str = include_str!("client/dist/index.html");
 const DEPS_JS: &str = include_str!("client/dist/agx_debug.js");
 const DEPS_CSS: &str = include_str!("client/dist/agx_debug.css");
+const FAVICON: &[u8] = include_bytes!("client/assets/favicon.png");
 
 pub struct DebugServer {
     debug_rx: DebugEventReceiver,
@@ -37,6 +38,7 @@ impl DebugServer {
             .route("/debug", get(root_get))
             .route("/agx_debug.js", get(js_get))
             .route("/agx_debug.css", get(css_get))
+            .route("/favicon.png", get(favicon_get))
             .route(EVENTS_PATH, get(sse_handler))
             .with_state(self.debug_rx.clone())
             .layer(cors);
@@ -87,6 +89,18 @@ async fn css_get() -> impl IntoResponse {
     );
 
     (headers, DEPS_CSS.to_string())
+}
+
+async fn favicon_get() -> impl IntoResponse {
+    let mut headers = HeaderMap::new();
+    #[allow(clippy::expect_used)]
+    headers.insert(
+        "Content-Type",
+        "image/png"
+            .parse()
+            .expect("content-type header value should've been parsed"),
+    );
+    (headers, FAVICON)
 }
 
 async fn sse_handler(
